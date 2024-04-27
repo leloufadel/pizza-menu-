6a93114496481f2bbf7486cf880aa7619d7713dc
leloufadel <leloufadel@gmail.com>
2024-04-27 14:51:30 +0000

    Add props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-    <h2><strong><Pizza /></strong> </h2> 
+      <Pizza />
+      <Menu />
     </div>
   );
-  function Pizza(){
-    return(
-      <div>Pizza</div>
-    )
+  function Menu() {
+    return (
+      <main className="menu">
+        <h2> Our Menu</h2>
+        <Pizza
+          name="Pizza Spinaci"
+          ingredient="Tomatto, mozorela, spinish"
+          photoName="pizzas/spinaci.jpg"
+          price="10.00"
+        />
+           <Pizza
+          name="Pizza Salamino "
+          ingredient="Tomatto, mozorela, spinish"
+          photoName="pizzas/salamino.jpg"
+          price="15.00"
+        />
+      </main>
+    );
+  }
+  function Pizza(props) {
+    return (
+      <>
+        {" "}
+        <img src={props.photoName} alt ={props.name}/>
+        <h2>${props.name}</h2>
+        <p>${props.ingredient}</p>
+        <p>${props.price}</p>
+
+      </>
+    );
   }
 }
 
